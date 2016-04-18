4a2cade2eb583bb2dede00f4436e1f8e4270cebc
Vineet Jain <jain.vinee@gmail.com>
2016-04-18 15:20:00 -0700

    initial graph

---
+++ graph/DirectedGraph.java
@@ -0,0 +1,12 @@
+package graph;
+
+/**
+ * DirectedGraph.java
+ *
+ * Created by Vineet Jain on 4/18/16.
+ *
+ * Representation of a Directed Graph that can have weighted edges, or
+ * unweighted edges depending on the intended use.
+ */
+public class DirectedGraph {
+}
---
+++ graph/Graph.java
@@ -0,0 +1,25 @@
+package graph;
+
+/**
+ * Graph.java
+ *
+ * Created by Vineet Jain on 4/18/16.
+ *
+ * Defines an interface that all Graph classes should follow
+ */
+public interface Graph {
+
+    /**
+     * Returns number of edges in this graph
+     * @return the number of edges in the graph
+     */
+    int numEdges();
+
+    /**
+     * Returns the number of vertices in this graph
+     * @return the number of verticies in the graph
+     */
+    int numVertices();
+
+
+}
