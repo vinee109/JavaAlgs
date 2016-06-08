94892ad0434898b794e3c2c87524cc980e6aa5ef
Vineet Jain <jain.vinee@gmail.com>
2016-06-07 23:22:18 -0700

    unit test for Singly Linked List

---
+++ list/SinglyLinkedListTest.java
@@ -0,0 +1,40 @@
+package list;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * list.SinglyLinkedListTest.java
+ *
+ * Created by Vineet Jain on 6/7/16.
+ *
+ * Unit Test for Singly Linked List
+ */
+public class SinglyLinkedListTest {
+
+    @Test
+    public void testInsertFront() throws Exception {
+
+    }
+
+    @Test
+    public void testInsertEnd() throws Exception {
+
+    }
+
+    @Test
+    public void testRemoveFront() throws Exception {
+
+    }
+
+    @Test
+    public void testRemoveEnd() throws Exception {
+
+    }
+
+    @Test
+    public void testIterator() throws Exception {
+
+    }
+}
