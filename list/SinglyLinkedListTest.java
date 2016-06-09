1155668cfe0945cb10812f499bf6042d5e20a406
Vineet Jain <jain.vinee@gmail.com>
2016-06-08 17:24:41 -0700

    more tests for SinglyLinkedList

--- list/SinglyLinkedListTest.java
+++ list/SinglyLinkedListTest.java
@@ -2,6 +2,10 @@ package list;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.NoSuchElementException;
+
 import static org.junit.Assert.*;
 
 /**
@@ -14,18 +18,80 @@ import static org.junit.Assert.*;
 public class SinglyLinkedListTest {
 
     @Test
-    public void testInsertFront() throws Exception {
+    public void testIsEmpty() throws Exception {
+        SinglyLinkedList<Integer> list = new SinglyLinkedList<>();
+        assertTrue(list.isEmpty());
+        assertEquals(0, list.size());
+    }
 
+    @Test
+    public void testInsertFront() throws Exception {
+        SinglyLinkedList<Integer> list = new SinglyLinkedList<>();
+        list.insertFront(1);
+        assertEquals("[ 1 ]", list.toString());
+        list.insertFront(2);
+        assertEquals("[ 2 1 ]", list.toString());
+        list.insertFront(3);
+        assertEquals("[ 3 2 1 ]", list.toString());
+        list.insertFront(4);
+        assertEquals("[ 4 3 2 1 ]", list.toString());
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testInsertFrontNull() throws Exception {
+        SinglyLinkedList<Integer> list = new SinglyLinkedList<>();
+        list.insertFront(null);
     }
 
     @Test
     public void testInsertEnd() throws Exception {
+        SinglyLinkedList<Integer> list = new SinglyLinkedList<>();
+        list.insertEnd(1);
+        assertEquals("[ 1 ]", list.toString());
+        assertEquals(1, list.size());
+        list.insertEnd(2);
+        assertEquals("[ 1 2 ]", list.toString());
+        assertEquals(2, list.size());
+        list.insertEnd(3);
+        assertEquals("[ 1 2 3 ]", list.toString());
+        assertEquals(3, list.size());
+        list.insertEnd(3);
+        assertEquals("[ 1 2 3 3 ]", list.toString());
+        assertEquals(4, list.size());
+    }
 
+    @Test(expected = NullPointerException.class)
+    public void testInsertEndNull() throws Exception {
+        SinglyLinkedList<Integer> list = new SinglyLinkedList<>();
+        list.insertEnd(null);
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void testRemoveFrontEmpty() throws Exception {
+        SinglyLinkedList<Integer> empty = new SinglyLinkedList<>();
+        empty.removeFront();
     }
 
     @Test
     public void testRemoveFront() throws Exception {
-
+        SinglyLinkedList<Integer> list = new SinglyLinkedList<>();
+        list.insertEnd(1);
+        list.insertEnd(2);
+        list.insertEnd(2);
+        list.insertEnd(2);
+        assertEquals("[ 1 2 2 2 ]", list.toString());
+        assertEquals(new Integer(1), list.removeFront());
+        assertEquals(3, list.size());
+        assertEquals("[ 2 2 2 ]", list.toString());
+        assertEquals(new Integer(2), list.removeFront());
+        assertEquals(2, list.size());
+        assertEquals("[ 2 2 ]", list.toString());
+        assertEquals(new Integer(2), list.removeFront());
+        assertEquals(1, list.size());
+        assertEquals("[ 2 ]", list.toString());
+        assertEquals(new Integer(2), list.removeFront());
+        assertEquals(0, list.size());
+        assertEquals("[  ]", list.toString());
     }
 
     @Test
@@ -35,6 +101,26 @@ public class SinglyLinkedListTest {
 
     @Test
     public void testIterator() throws Exception {
+        SinglyLinkedList<Integer> list = new SinglyLinkedList<>();
+        list.insertFront(1);
+        list.insertFront(2);
+        list.insertFront(3);
+        List<Integer> expected = Arrays.asList(3, 2, 1);
+        int i = 0;
+        for (Integer actual: list) {
+            assertEquals(expected.get(i++), actual);
+        }
 
+    }
+
+    @Test
+    public void testToString() throws Exception {
+        SinglyLinkedList<Integer> list = new SinglyLinkedList<>();
+        list.insertFront(1);
+        assertEquals("[ 1 ]", list.toString());
+        list.insertFront(2);
+        assertEquals("[ 2 1 ]", list.toString());
+        list.insertFront(3);
+        assertEquals("[ 3 2 1 ]", list.toString());
     }
 }
