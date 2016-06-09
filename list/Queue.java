1fae61b16e4b0a1b6fbcceb5ee0cc201fec4254c
Vineet Jain <jain.vinee@gmail.com>
2016-06-09 16:50:24 -0700

    Queue and Stack implementations

--- list/Queue.java
+++ list/Queue.java
@@ -1,7 +1,75 @@
 package list;
 
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
 /**
+ * list.Queue.java
+ *
  * Created by Vineet Jain on 6/8/16.
+ *
+ * The Queue class implements a First In First Out (FIFO). It supports
+ * enqueue and dequeue operations.
  */
-public class Queue {
+
+public class Queue<T> implements Iterable<T>{
+
+    private DoublyLinkedList<T> list;
+
+    public Queue() {
+        list = new DoublyLinkedList<>();
+    }
+
+    /**
+     * Enqueues an item to back of the queue.
+     * @param item the item to be enqueued
+     * @throws NullPointerException if item is null
+     */
+    public void enqueue(T item) {
+        if (item == null) {
+            throw new NullPointerException();
+        }
+        list.insertFront(item);
+    }
+
+    /**
+     * Dequeues an item from the front of the queue.
+     * @return the item that was dequeued
+     * @throws NoSuchElementException if the queue is empty.
+     */
+    public T dequeue() {
+        if (isEmpty()) {
+            throw new NoSuchElementException();
+        }
+        return list.removeEnd();
+    }
+
+    /**
+     * Returns the number of items currently in the queue.
+     * @return the number of items currently in the queue
+     */
+    public int size() {
+        return list.size();
+    }
+
+    /**
+     * Returns whether this queue is empty or not.
+     * @return true if the queue is emtpy, false otherwise
+     */
+    public boolean isEmpty() {
+        return list.isEmpty();
+    }
+
+    /**
+     * String representation of the queue
+     * @return a string representation of the items in this queue
+     */
+    public String toString() {
+        return list.toString();
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return list.iterator();
+    }
 }
--- list/Stack.java
+++ list/Stack.java
@@ -1,7 +1,69 @@
 package list;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
 /**
+ * list.Stack.java
+ *
  * Created by Vineet Jain on 6/8/16.
+ *
+ * The Stack class implements a Last-In First-Out (LIFO) Stack data structure.
+ * It supports the push and pop operations of a stack.
  */
-public class Stack {
+public class Stack<T> implements Iterable<T> {
+    private ArrayList<T> list;
+
+    public Stack() {
+        list = new ArrayList<>();
+    }
+
+    /**
+     * Pushes an item onto the stack.
+     * @param item the item to be pushed
+     * @throws NullPointerException if item is null
+     */
+    public void push(T item) {
+        if (item == null) {
+            throw new NullPointerException();
+        }
+        list.add(item);
+    }
+
+    /**
+     * Pops an item off the stack
+     * @return the item that was popped off
+     * @throws NoSuchElementException if the stack is empty
+     */
+    public T pop() {
+        if (isEmpty()) {
+            throw new NoSuchElementException();
+        }
+        return list.remove(size() - 1);
+    }
+
+    /**
+     * Returns the number of items in the stack
+     * @return the number of items in the stack
+     */
+    public int size() {
+        return list.size();
+    }
+
+    /**
+     * Returns whether this stack is empty or not
+     * @return true if the stack is empty, false otherwise
+     */
+    public boolean isEmpty() {
+        return list.isEmpty();
+    }
+
+    public String toString(){
+        return list.toString();
+    }
+
+    public Iterator<T> iterator() {
+        return list.iterator();
+    }
 }
